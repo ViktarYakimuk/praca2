0e6ce69e080ec92b1aa644f191e3b410267ca19c
ViktarYakimuk <ViktarYakimuk>
2022-09-04 13:40:24 +0300

    не доделал

---
+++ prc004/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+//14 -> нет 
+//46 -> нет 
+//161 -> да
+
+int Prompt(string message)
+{
+   Console.Write(message);               //вывод приглашения.
+   string strValue;                      //обьявдения для ввода строки
+   strValue = Console.ReadLine() ?? "0"; //вводими строку в консоли.(с консоли можно ввести только строку.)
+   int value = int.Parse(strValue);      //преоброзование строки в целое число.
+   return value;
+}
+
+
+System.Console.WriteLine("Введите число->");
+int a = int.Parse(Console.WriteLine()!);
+bool remains = (a % 7 == 0) && (a % 23 == 0);
+
+if (remains)
+{
+   Console.WriteLine("Кратно");
+}
+else
+{
+   Console.WriteLine("Не кратно");
+}
